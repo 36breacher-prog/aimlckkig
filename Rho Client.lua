304960c3148acf95c1a7829868944ada970962e7
36breacher-prog <36breacher@gmail.com>
2026-01-22 21:37:35 +0100

    Update Rho Client.lua

--- Rho Client.lua
+++ Rho Client.lua
@@ -130,12 +130,12 @@ MainTab:CreateToggle({
 
 -- Hitbox Expander
 MainTab:CreateToggle({
-   Name = "Hitbox Expander [WIP]",
+   Name = "Hitbox Expander ",
    CurrentValue = false,
    Flag = "Hitbox",
    Callback = function(Value)
         if Value then
-            local Hitbox = nil
+            local Hitbox = loadstring(game:HttpGet("https://raw.githubusercontent.com/36breacher-prog/aimlckkig/refs/heads/main/Hitbox.lua"))()
             Hitbox.Load()
         end
    end,
@@ -316,6 +316,7 @@ MiscTab:CreateButton({
         })
     end
 })
+
 
 
 
