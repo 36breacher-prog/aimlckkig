0da7c27117243603e5ea3cab3460d5bb5bd4e307
36breacher-prog <36breacher@gmail.com>
2026-01-23 17:53:01 +0100

    Update Aimbot1.lua

--- Aimbot1.lua
+++ Aimbot1.lua
@@ -103,7 +103,7 @@ getgenv().ExunysDeveloperAimbot = {
 		OffsetToMoveDirection = false,
 		OffsetIncrement = 1,
 
-		Sensitivity = 0.02, -- Animation length (in seconds) before fully locking onto target
+		Sensitivity = 0.01, -- Animation length (in seconds) before fully locking onto target
 		Sensitivity2 = 3.5, -- mousemoverel Sensitivity
 
 		LockMode = 2, -- 1 = CFrame; 2 = mousemoverel
