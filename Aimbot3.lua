dc1cf9f45ed7a7fa2d2037f2e9b717e14f7b42ae
36breacher-prog <36breacher@gmail.com>
2026-03-04 20:12:58 +0100

    Update Aimbot3.lua

--- Aimbot3.lua
+++ Aimbot3.lua
@@ -104,7 +104,7 @@ getgenv().ExunysDeveloperAimbot = {
 		OffsetIncrement = 2.5,
 
 		Sensitivity = 0.025, -- Animation length (in seconds) before fully locking onto target
-		Sensitivity2 = 8.5, -- mousemoverel Sensitivity
+		Sensitivity2 = 3.8, -- mousemoverel Sensitivity
 
 		LockMode = 2, -- 1 = CFrame; 2 = mousemoverel
 		LockPart = "Head", -- Body part to lock on
